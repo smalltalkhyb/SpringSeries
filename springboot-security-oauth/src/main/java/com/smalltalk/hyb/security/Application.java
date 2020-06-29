879a2a16b359c61b08acc55de9256ef28ff4a5e5
huyanbing <huyanbing@vipkid.com.cn>
2020-06-29 10:53:08 +0800

    修改改名

---
+++ springboot-security-oauth/src/main/java/com/smalltalk/hyb/security/Application.java
@@ -0,0 +1,12 @@
+package com.smalltalk.hyb.security;
+
+/**
+ * @author: ybhu
+ * @create: 2020/06/27 16:59
+ */
+public class Application {
+
+    public static void main(String[] args){
+        new SpringApplication(Application.class).run(args);
+    }
+}
